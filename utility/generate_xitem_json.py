52c8689861ddcf634c98ad48a578f57f45ff4a61
Rahul Kunwar <rkunwark@xilinx.com>
2020-08-14 21:43:21 +0530

    Fix for python linting issues

--- utility/generate_xitem_json.py
+++ utility/generate_xitem_json.py
@@ -5,104 +5,104 @@ from xml.dom import minidom
 from collections import OrderedDict
 
 def createXitemJson(xmldoc,args) :
-	itemlist = xmldoc.getElementsByTagName('board')
-	length = len(itemlist) 
+    itemlist = xmldoc.getElementsByTagName('board')
+    length = len(itemlist) 
 
-	if length > 0:
-		name = itemlist[0].attributes['name'].value
-		dsname = itemlist[0].attributes['display_name'].value
-		vendor = itemlist[0].attributes['vendor'].value	
-		url = itemlist[0].attributes['url'].value
-		schema_version = itemlist[0].attributes['schema_version'].value
-		#if (schema_version < "2.0"):
-	#		print ("The utility script does work for schema 2.0 only")
-	#		exit()
-	else: 
-		print ("Did not find board Specific data in file , cannot Create xitem json file")
-		exit()
+    if length > 0:
+        name = itemlist[0].attributes['name'].value
+        dsname = itemlist[0].attributes['display_name'].value
+        vendor = itemlist[0].attributes['vendor'].value    
+        url = itemlist[0].attributes['url'].value
+        # schema_version = itemlist[0].attributes['schema_version'].value
+        # if (schema_version < "2.0"):
+        #    print ("The utility script does work for schema 2.0 only")
+        #    exit()
+    else: 
+        print ("Did not find board Specific data in file , cannot Create xitem json file")
+        exit()
 
-	subtype = "board"	
-	imagelist = xmldoc.getElementsByTagName('image')
+    subtype = "board"    
+    imagelist = xmldoc.getElementsByTagName('image')
 
-	imlength = len(imagelist)
-        if imlength > 0:
-           logo = imagelist[0].attributes['name'].value
-  
-        deslist = xmldoc.getElementsByTagName("description")	
-	deslen = len(deslist)
+    imlength = len(imagelist)
+    if imlength > 0:
+        logo = imagelist[0].attributes['name'].value
+    
+    deslist = xmldoc.getElementsByTagName("description")
+    deslen = len(deslist)
 
-	if deslen > 0 :
-		des = deslist[imlength].firstChild
-		if des:
-			descr = des.nodeValue.strip()
-	filever = xmldoc.getElementsByTagName("file_version")
-	
-	for filev in filever:
-		child = (filev.firstChild)
-		if child:
-			ver = child.nodeValue.strip()
-		else :
-			print ("Did not finf file_version in the board file")
-			exit()
-	
-	complist = xmldoc.getElementsByTagName("component")
-	fpgacount = 0
-	category = "Single Part"         
-	for compnode in complist:
-		comp_type = compnode.attributes['type'].value
-		if comp_type == "fpga":
-			fpgacount+=1
+    if deslen > 0 :
+        des = deslist[imlength].firstChild
+        if des:
+            descr = des.nodeValue.strip()
+    filever = xmldoc.getElementsByTagName("file_version")
+    
+    for filev in filever:
+        child = (filev.firstChild)
+        if child:
+            ver = child.nodeValue.strip()
+        else :
+            print ("Did not finf file_version in the board file")
+            exit()
+    
+    complist = xmldoc.getElementsByTagName("component")
+    fpgacount = 0
+    category = "Single Part"         
+    for compnode in complist:
+        comp_type = compnode.attributes['type'].value
+        if comp_type == "fpga":
+            fpgacount+=1
 
-	if fpgacount > 1 :
-		category = "Multi part"
+    if fpgacount > 1 :
+        category = "Multi part"
 
-	data = OrderedDict()
-	config = OrderedDict()
-	search_keywords = [name,vendor,subtype,category]
-	
-	orderItem = OrderedDict()
-	company_name = vendor
-	if (args.company_display_name !=""):
-		vendor = args.company_display_name	
-	
-	orderItem["name"]  = name
-	orderItem["display"] = dsname
-	orderItem["revision"] = ver
-	orderItem["description"] = descr
-	orderItem["company"] = company_name
-	orderItem["company_display"] = vendor
-	orderItem["author"] = args.author
-	
-	contributor = OrderedDict()
-	
-	contributor['group'] = vendor
-	contributor["url"] = args.company_url
-	contributors = [contributor]
-	
-	orderItem["contributors"] = contributors
-	orderItem["category"] = category
-	orderItem["website"] = url
-	if logo != "":
-		orderItem["logo"] = logo
-	orderItem["search-keywords"] = search_keywords
-	
-	item = OrderedDict()
-	item["infra"] = orderItem
-	
-	orderItems = [item]
-	config['items'] = orderItems
-		
-	data['config'] = config
-	data["_major"] = 1
-	data["_minor"] = 0
+    data = OrderedDict()
+    config = OrderedDict()
+    search_keywords = [name,vendor,subtype,category]
+    
+    orderItem = OrderedDict()
+    company_name = vendor
+    if (args.company_display_name !=""):
+        vendor = args.company_display_name    
+    
+    orderItem["name"]  = name
+    orderItem["display"] = dsname
+    orderItem["revision"] = ver
+    orderItem["description"] = descr
+    orderItem["company"] = company_name
+    orderItem["company_display"] = vendor
+    orderItem["author"] = args.author
+    
+    contributor = OrderedDict()
+    
+    contributor['group'] = vendor
+    contributor["url"] = args.company_url
+    contributors = [contributor]
+    
+    orderItem["contributors"] = contributors
+    orderItem["category"] = category
+    orderItem["website"] = url
+    if logo != "":
+        orderItem["logo"] = logo
+    orderItem["search-keywords"] = search_keywords
+    
+    item = OrderedDict()
+    item["infra"] = orderItem
+    
+    orderItems = [item]
+    config['items'] = orderItems
+        
+    data['config'] = config
+    data["_major"] = 1
+    data["_minor"] = 0
 
-	try:	
-		outfile = open(args.output_file,'w')
-	except IOError:
-		print ('cannot open', args.output_file)
-	else:
-		json.dump(data, outfile,indent = 2)
-		outfile.close()
+    try:    
+        outfile = open(args.output_file,'w')
+    except IOError:
+        print ('cannot open', args.output_file)
+    else:
+        json.dump(data, outfile,indent = 2)
+        outfile.close()
 
 def parse_cmdline():
 
@@ -117,15 +117,15 @@ def parse_cmdline():
     return parser
 
 def main():
-	parser = parse_cmdline()
-	args = parser.parse_args() 
-	try : 
-		infile= open(args.board_file,"r")
-	except IOError:
-		print ('cannot open', args.board_file)
-	else:
-		infile.close()
-		xmldoc = minidom.parse(args.board_file)	
-		createXitemJson(xmldoc,args)
+    parser = parse_cmdline()
+    args = parser.parse_args() 
+    try : 
+        infile= open(args.board_file,"r")
+    except IOError:
+        print ('cannot open', args.board_file)
+    else:
+        infile.close()
+        xmldoc = minidom.parse(args.board_file)    
+        createXitemJson(xmldoc,args)
 
 if __name__ == '__main__': main()
